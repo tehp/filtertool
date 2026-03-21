c4bd3ef5a1f26b15b27bbd27cfd284c3a5e842d4
Moritz T <mositho97@gmail.com>
2026-03-21 02:24:40 +0100

    ignore autogenerated stuff v2

--- src/sounds/paths.ts
+++ src/sounds/paths.ts
@@ -1,5 +1,4 @@
 import path from "path"
-import type { SoundFile } from "../types/sounds/generated-sounds"
 
 export const SOUND_PACK_SOURCE_DIR = "sounds"
 const SOUND_PACK_TARGET_DIR = "poeft-sounds"
@@ -8,7 +7,6 @@ const normalizeFolder = (folder: string) => folder.replace(/^[\\/]+|[\\/]+$/g, "
 
 export const getSoundPackFolder = () => normalizeFolder(process.env.SOUNDS_FOLDER || SOUND_PACK_TARGET_DIR)
 
-export function soundFile(file: SoundFile): string
 export function soundFile(file: string): string
 export function soundFile(file: string) {
   return `${getSoundPackFolder()}/${file}`
